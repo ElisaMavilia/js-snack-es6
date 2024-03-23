a7b8b1bbf84024ee5a845fa58f0c5cae141a1a23
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-23 13:44:00 +0100

    new list

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ Generiamo e stampiamo in console la lista per i segnaposto. */
 
 let vips = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
+
+
 let guest = [
     {
         tableName: "Tavolo Vip",
@@ -19,71 +21,23 @@ let guest = [
 ]
 
 vips.forEach((element, index) =>{
+let guestName = element;
+let tableName = "Tavolo vip";
+let place = index + 1;
 const table = [
     {
-        tableName: "",
-        guestName: "",
-        place: ""
+        tableName,
+        guestName,
+        place
     }
 ]
+guest.push(table, element, index); 
+console.log(table, element, index);
 
-table.filter (element => {
-    return element.guestName == "";
+
 })
-
-console.log(table);
-})
-
-
-
-    
-
- 
 console.log("guests:", guest);
 
-
-
-
-/* guest[1].guestName = 'Johnny Depp'; 
-guest[1].place = 2;
-guest[1].tableName = "Tavolo Vip";
-
-guest[2].guestName = 'Lady Gaga';
-guest[2].place = 3;
-guest[2].tableName = "Tavolo Vip";
-
-guest[3].guestName = 'Cristiano Ronaldo';
-guest[3].place = 4;
-guest[3].tableName = "Tavolo Vip";
-
-guest[4].guestName = 'Georgina Rodriguez';
-guest[4].place = 5;
-guest[4].tableName = "Tavolo Vip";
-
-guest[5].guestName = 'Chiara Ferragni';
-guest[5].place = 6;
-guest[5].tableName = "Tavolo Vip";
-
-guest[6].guestName = 'George Clooney';
-guest[6].place = 7;
-guest[6].tableName = "Tavolo Vip";
-
-guest[7].guestName = 'Amal Clooney';
-guest[7].place = 8;
-guest[7].tableName = "Tavolo Vip";
-
-guest[8].guestName = 'Fedez';
-guest[8].place = 9;
-guest[8].tableName = "Tavolo Vip";
-
-guest[9].guestName = 'Amadeus';
-guest[9].place = 9;
-guest[9].tableName = "Tavolo Vip";
-
-guest[10].guestName = 'Fiorello';
-guest[10].place = 10;
-guest[10].tableName = "Tavolo Vip"; 
- */
 
 
 
